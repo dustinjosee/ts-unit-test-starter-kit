d04b22bddb3d1512aeeb352654611cf20a8903ed
Dustin Josee Susilo <susilod@student.unimelb.edu.au>
2024-08-21 13:52:01 +1000

    add testing

--- tests/ecommerce.test.ts
+++ tests/ecommerce.test.ts
@@ -2,11 +2,12 @@ import { describe, it, expect, beforeEach } from "vitest";
 import {
   addProduct,
   addToCart,
+  applyShippingDiscount,
   calculateTotal,
   clearCart,
 } from "../src/ecommerce";
 
-let cart = {};
+
 describe("E-commerce System", () => {
   beforeEach(() => {
     addProduct("Soap", 100, 10);
@@ -15,9 +16,43 @@ describe("E-commerce System", () => {
   });
 
   it("should calculate price of all products", () => {
+    // Arrange 
     addToCart("Soap", 2);
     addToCart("Shampoo", 2);
+
+    // Act
+
     const sum = calculateTotal();
+
+    // Assert
     expect(sum).toBe(600);
   });
+
+  it("should add items to cart", () => {
+    // Arrange
+    let cart = {};
+    // Act
+    cart = addToCart("Soap", 2);
+    // Assert
+    expect(cart["Soap"]).toBe(2); 
+  })
+
+  it("should apply shipping discount", () => {
+    addToCart("Soap", 1);
+    addToCart("Shampoo", 2)
+
+    const sum = calculateTotal();
+
+    expect(applyShippingDiscount(sum)).toBe(490);
+
+  })
+
+  it("should not apply shipping discount", () => {
+    addToCart("Soap", 1);
+    addToCart("Shampoo", 1)
+
+    const sum = calculateTotal();
+
+    expect(applyShippingDiscount(sum)).toBe(300);
+  })
 });
